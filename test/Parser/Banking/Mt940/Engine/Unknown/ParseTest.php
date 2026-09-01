823836934dbaebb0d3c634b822998e50dfe7d125
David Schilling <dschilling@churchtools.de>
2026-09-01 14:21:48 +0200

    Parse IBAN from structured purpose fields

--- test/Parser/Banking/Mt940/Engine/Unknown/ParseTest.php
+++ test/Parser/Banking/Mt940/Engine/Unknown/ParseTest.php
@@ -30,5 +30,16 @@ class ParseTest extends TestCase
         self::assertEquals('50092100', $this->statements[0]->getBankCode());
         self::assertEquals('1782002', $this->statements[0]->getAccountNumber());
         self::assertEquals('acfbc24843a9b17f3eb8d316ac10694d0787304202254c5d1bee62f537c18048', $this->statements[0]->getTransactions()[0]->getFingerprint());
+        self::assertEquals('DE99327719624505558948', $this->statements[0]->getTransactions()[0]->getAccount());
+    }
+
+    public function testParsesIbanFromPurposeFieldsWhenAccountFieldIsMissing()
+    {
+        $engine = new Unknown();
+        $engine->loadString(file_get_contents(__DIR__.'/embedded-iban-sample'));
+
+        $statements = $engine->parse();
+
+        self::assertEquals('DE43123456789012345678', $statements[0]->getTransactions()[0]->getAccount());
     }
 }
